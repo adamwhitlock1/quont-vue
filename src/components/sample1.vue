2fd9b1477b94d00b44f0dc99be0196ae2a8eb842
Adam Whitlock <moose62712@gmail.com>
2018-11-21 23:04:22 -0700

    added local forage and persistance

--- src/components/sample1.vue
+++ src/components/sample1.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="hello">
+    <div :class="[modalActive ? 'modal is-active' : 'modal']">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Welcome back to quon't.</p>
+        </header>
+        <section class="modal-card-body">
+          <h2 class="title">
+            We have found existing quotes from the past that you have saved.
+            Would you like to load these quotes or start with new, fresh quotes?
+          </h2>
+        </section>
+        <footer class="modal-card-foot">
+          <button @click="loadSavedQuotes" class="button is-success">
+            Load em up
+          </button>
+          <button @click="loadFreshies" class="button">
+            Start with freshies
+          </button>
+        </footer>
+      </div>
+    </div>
+
     <div class="container">
       <div class="columns">
         <div class="column">
@@ -76,7 +99,7 @@
 
       <div class="container">
         <draggable
-          v-model="items"
+          v-model="items.slice().reverse()"
           :options="{ group: 'projects', delay: 150 }"
           @start="drag = true;"
           @end="drag = false;"
@@ -107,7 +130,7 @@
                     >
                       <div class="columns">
                         <div class="column">
-                          <textarea v-model="editText" class="input" />
+                          <textarea v-model="editText" class="textarea" />
                         </div>
                         <div class="column is-narrow">
                           <button class="button is-small is-primary">
@@ -157,6 +180,13 @@
           </div>
         </draggable>
       </div>
+      <div class="container">
+        <div class="column is-4 is-offset-8">
+          <button @click="loadFreshies" class="button is-danger">
+            Delete all your quotes and get 3 new quotes.
+          </button>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -164,45 +194,102 @@
 <script>
 import draggable from "vuedraggable";
 import moment from "moment";
+
 export default {
   name: "HelloWorld",
+
+  mounted() {
+    this.$vf.config({
+      name: "vue-forage"
+    });
+
+    this.$vf
+      .getItem("quotes")
+      .then(value => {
+        // This code runs once the value has been loaded
+        // from the offline store.
+        console.log(value);
+        if (value === null) {
+          console.log("No local storage entries present");
+        } else {
+          this.modalActive = true;
+        }
+      })
+      .catch(err => {
+        // This code runs if there were any errors
+        console.log(err);
+      });
+
+    // this.$vf.setItem("app", "Vue Forage");
+  },
+
   components: {
     draggable
   },
   data() {
     return {
-      items: [
-        {
-          text: "test text a",
-          author: "Some Author",
-          cat: "A category",
-          date: moment().format("MMMM Do YYYY, h:mm:ss a")
-        },
-        {
-          text: "test text b",
-          author: "Some Author",
-          cat: "A category",
-          date: moment().format("MMMM Do YYYY, h:mm:ss a")
-        },
-        {
-          text: "test text c",
-          author: "Some Author",
-          cat: "A category",
-          date: moment().format("MMMM Do YYYY, h:mm:ss a")
-        }
-      ],
-      items2: [],
+      items: [],
       newAuthor: "",
       newCat: "",
       newText: "",
       editText: "",
       editing: 0,
       addingTodo: false,
-      quoteLoading: true
+      quoteLoading: true,
+      modalActive: false
     };
   },
 
   methods: {
+    openLoadQuotesModal() {
+      this.$modal.show("load-quotes-modal");
+    },
+
+    loadFreshies() {
+      this.items = [];
+      for (let i = 0; i < 3; i++) {
+        this.$http.get("https://talaikis.com/api/quotes/random/").then(
+          response => {
+            // get body data
+            let quote = {
+              text: response.body.quote,
+              author: response.body.author,
+              cat: response.body.cat,
+              date: moment().format("MMMM Do YYYY, h:mm:ss a")
+            };
+
+            this.items.push(quote);
+          },
+          response => {
+            // error callback
+          }
+        );
+      }
+      this.modalActive = false;
+    },
+
+    loadSavedQuotes() {
+      console.log("Loading saved quotes");
+      this.$vf
+        .getItem("quotes")
+        .then(value => {
+          // This code runs once the value has been loaded
+          // from the offline store.
+          console.log(value);
+          if (value === null) {
+            console.log("No local storage entries present");
+          } else {
+            this.items = value;
+          }
+        })
+        .catch(err => {
+          // This code runs if there were any errors
+          console.log(err);
+        });
+
+      this.modalActive = false;
+    },
+
     newTodo() {
       this.addingTodo = this.addingTodo === true ? false : true;
       if (this.addingTodo === true) {
@@ -227,6 +314,7 @@ export default {
         author: this.newAuthor,
         cat: this.newCat
       });
+      this.$vf.setItem("quotes", this.items);
       this.quoteLoading = true;
       this.$http.get("https://talaikis.com/api/quotes/random/").then(
         response => {
@@ -247,15 +335,19 @@ export default {
       let itemToDel = this.items.length - index - 1;
       console.log(itemToDel);
       this.items.splice(itemToDel, 1);
+      this.$vf.setItem("quotes", this.items);
     },
     editTodo(index) {
+      let itemToedit = this.items.length - index - 1;
       this.editing = this.editing === 0 ? index + 1 : 0;
-      this.editText = this.items[index].text;
+      this.editText = this.items[itemToedit].text;
     },
     commitEdit(index) {
-      this.items[index].text = this.editText;
+      let itemToedit = this.items.length - index - 1;
+      this.items[itemToedit].text = this.editText;
       this.editText = "";
       this.editing = 0;
+      this.$vf.setItem("quotes", this.items);
     },
     cancelEdit() {
       this.editing = 0;
